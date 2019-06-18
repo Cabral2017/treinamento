90d984c7c9484d951340f1cd906f126c39c96f67
André Wallison Cabral <andre.cabral@digicade.corp>
2019-06-18 16:39:30 -0300

    Alterado menu lateral

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -24,55 +24,34 @@
       v-model="leftDrawerOpen"
       bordered
       content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
+    > 
+    <template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-list link>
+        <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="teal" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Opção 1</q-item-label>
+          <!--<q-item-label caption>Selecione esta opção para ativar o layer 1</q-item-label>-->
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="orange" color="orange" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Opção 2</q-item-label>
+          <!--<q-item-label caption>Selecione esta opção para ativar o layer 2</q-item-label>-->
+        </q-item-section>
+      </q-item>
       </q-list>
+      </div>
+      </div>
+      </template>
     </q-drawer>
 
     <q-page-container>
@@ -88,7 +67,8 @@ export default {
   name: 'MyLayout',
   data () {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      color: ['orange']
     }
   },
   methods: {
---
+++ src/plugins/openlayers.js
@@ -0,0 +1,27 @@
+require('ol/ol.css')
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import ImageLayer from 'ol/layer/Image'
+import VectorLayer from 'ol/layer/Vector'
+import OSMSource from 'ol/source/OSM'
+import TileWMSSource from 'ol/source/TileWMS'
+import VectorSource from 'ol/source/Vector'
+import ImageWMSSource from 'ol/source/ImageWMS'
+import { fromLonLat } from 'ol/proj.js'
+export default ({ app, router, Vue }) => {
+    let ol = {}
+    ol.Map = Map
+    ol.View = View
+    ol.layer = {}
+    ol.layer.Tile = TileLayer
+    ol.layer.Image = ImageLayer
+    ol.layer.Vector = VectorLayer
+    ol.source = {}
+    ol.source.OSM = OSMSource
+    ol.source.TileWMS = TileWMSSource
+    ol.source.Vector = VectorSource
+    ol.source.ImageWMS = ImageWMSSource
+    ol.fromLonLat = fromLonLat
+    Vue.prototype.$ol = ol
+}
